416f06d705e720c826386369b187c7242e9c8a50
shandy254 <tirtabagus30@gmail.com>
2019-08-22 20:12:26 +0700

    pindahan dari desktop/informatika

---
+++ Python/Sekolah koding/pert1-10.py
@@ -0,0 +1,35 @@
+
+# B E R L A T I H   P Y T H O N
+# Sekolah Koding
+
+x=10
+y=20
+z=18
+n="kamu"
+o="suka"  
+m='a'
+dia=input("siapa anda? ")
+
+a = max(x,y,z)
+i = min(x,y,z)
+N = n.replace('k','t')
+O = o.capitalize()
+
+st = "7"
+# it = int(st)
+
+print("hayo apa\n" + N + " " + O)
+print (a, i)
+print(a + i)
+
+print("kamu" , i)
+print("ini adalah petik\" di dalam petik")
+print("mengubah " + str(a) + " menjadi string")
+# (error) print("mengubah " + int(st) + " menjadi integer")
+print("\n'backslash n' digunakan untuk 'enter',\n\t'backslash t' digunakan untuk 'tab' ")
+
+"""
+sekian, 
+yang di atas adalah video 1-10
+dari sekolah koding di youtube
+"""
